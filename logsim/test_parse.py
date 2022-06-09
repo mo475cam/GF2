0889a122aa0074cbcf54fe4a6e19b0755c931de8
mo475cam <105428974+mo475cam@users.noreply.github.com>
2022-06-09 14:20:41 +0100

    Debug mode created with assertion tests

--- logsim/test_parse.py
+++ logsim/test_parse.py
@@ -1,22 +1,11 @@
 import pytest
 from scanner import Scanner, Symbol
 from names import Names
+from devices import Devices
+from network import Network
+from monitors import Monitors
+from parse import Parser
+from names import Names
+from unittest import mock
 
 
-@pytest.fixture
-def symbol():
-    symbol = Symbol()
-    return symbol
-
-@pytest.fixture
-def scanner():
-    scanner = Scanner()
-    return scanner
-
-
-
-@pytest.fixture
-def test_devices_list(self):
-    self.devices_list()
-    assert self.symbol.type == self.scanner.KEYWORD or \
-            self.in_stopping_symbol is True
